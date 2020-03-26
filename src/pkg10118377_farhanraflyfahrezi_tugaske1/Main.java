a4ebd4ab46eded457dc507cb477aafd8953c4041
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-03-26 16:17:43 +0700

    first commit

---
+++ src/pkg10118377_farhanraflyfahrezi_tugaske1/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg10118377_farhanraflyfahrezi_tugaske1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
